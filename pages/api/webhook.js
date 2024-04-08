5a4d59c46b601c7f38afd7f254bc78dc5577682f
Caio <caio.freitas.brito@gmail.com>
2024-04-08 00:42:27 -0300

    API MP

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,4 +1,11 @@
 export default function handle(request, response) {
-    console.log(request);
+    
+    const data = request.query
+
+    const type = dados["type"] || dados["topic"]
+    const id = dados["data.id"] || dados["id"]
+
+    console.log(type, id)
+
     response.status(200)
 }
